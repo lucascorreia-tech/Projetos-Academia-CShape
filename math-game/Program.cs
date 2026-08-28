ca6fd3ab8afa88e02b3b9727c57ed1d109964b51
SuporteNtsParts <suporte@ntsparts.com.br>
2026-08-28 17:21:55 -0300

    feat: criacao da lógica de menu

---
+++ math-game/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int op = 0;
+        do
+        {
+            Console.WriteLine("Bem vindo ao Jogo de Matématico.");
+            Console.WriteLine("Escolha uma opção abaixo:");
+            Console.WriteLine("1- Jogo fácil\n0- Sair do jogo");
+            switch (op)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+        }while(op != 0);
+        
+    }
+
+}
+
