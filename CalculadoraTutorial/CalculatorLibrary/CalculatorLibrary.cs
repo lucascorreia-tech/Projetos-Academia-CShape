a235e5f44dab8fe2d759f4c5e3b1bcd40758503b
Lucas Correia <lucasrcorreia23@gmail.com>
2026-09-06 23:48:34 -0300

    feat: Novas Operaçoes

--- CalculadoraTutorial/CalculatorLibrary/CalculatorLibrary.cs
+++ CalculadoraTutorial/CalculatorLibrary/CalculatorLibrary.cs
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-﻿using System.IO.Pipelines;
-using System.Net;
-=======
-﻿// CalculatorLibrary.cs
->>>>>>> 6d975693133da94bccf3c827efe25fad3337113c
 using Newtonsoft.Json;
 
 namespace CalculatorLibrary
@@ -48,13 +42,10 @@ namespace CalculatorLibrary
                     result = num1 * num2;
                     writer.WriteValue("Multiply");
                     break;
-<<<<<<< HEAD
                 case "p":
                     result = Math.Pow(num1,num2);
                     writer.WriteValue("Pow");
                     break;
-=======
->>>>>>> 6d975693133da94bccf3c827efe25fad3337113c
                 case "d":
                     // Ask the user to enter a non-zero divisor.
                     if (num2 != 0)
@@ -74,7 +65,6 @@ namespace CalculatorLibrary
             return result;
         }
 
-<<<<<<< HEAD
         public double DoOtherOperation(double num1, string op)
         {
             double result = double.NaN;
@@ -112,8 +102,6 @@ namespace CalculatorLibrary
             return result;
         }
 
-=======
->>>>>>> 6d975693133da94bccf3c827efe25fad3337113c
         public void Finish()
         {
             writer.WriteEndArray();
